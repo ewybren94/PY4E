0b5ab493fbbb1c000d90096624c5245de5e94feb
ewybren94 <126259836+ewybren94@users.noreply.github.com>
2024-05-07 12:14:59 +0100

    Add files via upload
    Chapter 3 Exercise 1 from the PY4E book

---
+++ overtime.py
@@ -0,0 +1,7 @@
+Hours = input('Enter hours: ')
+Rate = input ('Enter rate: ')
+if int(Hours) <= 40:
+    Pay = int(Hours) * float(Rate)
+if int(Hours) >= 40:
+    Pay = (int(Hours) * float(Rate)) + (0.5 * float(Rate) * (int(Hours)-40))
+print('Pay:', Pay)
